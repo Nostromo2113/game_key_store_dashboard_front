d19d3f0eb0ad6db13fc6073cacc0da9dc1f86501
Yaroslav-Yurev <arklaid11@gmail.com>
2025-04-01 18:20:15 +0300

    decomposition ProductCards. add ProductCard component

--- src/components/blocks/ProductCards.vue
+++ src/components/blocks/ProductCards.vue
@@ -5,70 +5,15 @@
     </q-btn-dropdown>
 
     <div v-if="rows.length" class="product-grid">
-      <q-card
+      <ProductCard
         v-for="product in rows"
         :key="product.id"
-        class="flex justify-between gap-sm custom-rounded shadow-sm"
-        style="width: 284px"
-        flat
-      >
-        <q-img
-          :src="getImageUrl(product.preview_image)"
-          width="100%"
-          height="200px"
-          class="rounded-borders"
-        />
-
-        <q-card-section class="q-py-sm q-px-md full-width">
-          <div class="text-h6">{{ product.title }}</div>
-
-          <div class="text-subtitle2">{{ product.publisher }}</div>
-
-          <div>
-            <div class="text-subtitle1">Цена: {{ product.price }} ₽</div>
-            <div class="text-subtitle2">Количество: {{ product.amount }}</div>
-          </div>
-        </q-card-section>
-        <q-card-section class="q-py-none">
-          <div class="q-px-sm bg-primary text-white custom-rounded text-caption">
-            {{ product.category.title }}
-          </div>
-        </q-card-section>
-        <q-card-actions align="center" class="full-width">
-          <div class="row justify-between full-width">
-            <q-btn
-              flat
-              rounded
-              icon="visibility"
-              @click="viewProduct(product.id)"
-              label="Посмотреть"
-              no-caps
-            />
-
-            <q-btn
-              v-if="!isInCart(product.id)"
-              @click="storeProductToCart(cartId, product)"
-              color="primary"
-              flat
-              rounded
-              icon="shopping_cart"
-              no-caps
-            >
-              <q-tooltip v-if="isInCart(product.id)">В корзине</q-tooltip>
-            </q-btn>
-            <q-btn
-              v-if="isInCart(product.id)"
-              @click="removeProductFromCart(product.id)"
-              color="negative"
-              icon="remove_shopping_cart"
-              flat
-              rounded
-              no-caps
-            >
-            </q-btn>
-          </div>
-        </q-card-actions>
-      </q-card>
+        :product="product"
+        :is-in-cart="isInCart(product.id)"
+        @view="viewProduct"
+        @add-to-cart="storeProductToCart(cartId, $event)"
+        @remove-from-cart="removeProductFromCart"
+      />
     </div>
     <div v-if="hasMore" class="full-width flex justify-center">
       <q-btn @click="showMore" class="q-my-xl custom-rounded" color="info" :disable="!hasMore">
@@ -79,11 +24,11 @@
 </template>
 
 <script setup>
+import ProductCard from './ProductCard.vue'
 import FilterGroup from './FilterGroup.vue'
 import { onMounted, ref, computed, defineEmits } from 'vue'
 import { useRouter } from 'vue-router'
 import { getData } from 'src/utils/http/get'
-import { getImageUrl } from 'src/utils/getImageUrl'
 import { useCartStore } from 'src/stores/cartStore'
 import notify from 'src/plugins/notify'
 
