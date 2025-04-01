bd8637d61205548e794b47996d64b46fc71fb08f
Yaroslav-Yurev <arklaid11@gmail.com>
2025-04-01 17:35:20 +0300

    moved fetchCart method to ShopLayout

--- src/layouts/ShopLayout.vue
+++ src/layouts/ShopLayout.vue
@@ -21,17 +21,39 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, watch } from 'vue'
 import { useCartStore } from 'src/stores/cartStore'
 import { useRoute } from 'vue-router'
+import { useUserStore } from 'src/stores/userStore'
 
 const route = useRoute()
 
 const cartStore = useCartStore()
 
 const cartId = computed(() => cartStore.cartDetails.id)
+const userId = computed(() => useUserStore.user?.id)
 
 const cartLink = computed(() =>
   cartId.value ? { name: 'shop.cart', params: { cartId: cartId.value } } : { name: 'shop' },
+)
+
+const fetchCart = async (userId) => {
+  try {
+    await cartStore.fetchCart(userId)
+  } catch (e) {
+    console.error(e)
+  } finally {
+    console.log('ready', cartStore.cartProducts)
+  }
+}
+
+watch(
+  () => userId.value,
+  (newValue) => {
+    if (newValue) {
+      fetchCart(newValue)
+    }
+  },
+  { immediate: true },
 )
 </script>
--- src/pages/shop/ShopIndexPage.vue
+++ src/pages/shop/ShopIndexPage.vue
@@ -10,38 +10,11 @@
 </template>
 
 <script setup>
-import { ref, computed, watch } from 'vue'
+import { ref } from 'vue'
 import ProductCards from 'src/components/blocks/ProductCards.vue'
 import InnerLoading from 'src/components/ui/InnerLoading.vue'
-import { useUserStore } from 'src/stores/userStore'
-import { useCartStore } from 'src/stores/cartStore'
-
-const userStore = useUserStore()
-const cartStore = useCartStore()
-
-const userId = computed(() => userStore.user?.id)
 
 const loading = ref(true)
-
-const fetchCart = async (userId) => {
-  try {
-    await cartStore.fetchCart(userId)
-  } catch (e) {
-    console.error(e)
-  } finally {
-    console.log('ready', cartStore.cartProducts)
-  }
-}
-
-watch(
-  () => userId.value,
-  (newValue) => {
-    if (newValue) {
-      fetchCart(newValue)
-    }
-  },
-  { immediate: true },
-)
 </script>
 
 <style scoped></style>
