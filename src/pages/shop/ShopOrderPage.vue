833906dafbb11b331580e97e8eb7ec73b97c7129
Yaroslav-Yurev <arklaid11@gmail.com>
2025-04-08 17:33:24 +0300

    fix Exception to http post method. rename ShopProduct to ProductShop to ProductShopShow, OrderShop to OrderShopShow

--- src/pages/shop/ShopOrderPage.vue
+++ src/pages/shop/ShopOrderPage.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="relative-position page-height">
-    <OrderShop v-show="!loading" @success="loading = false" />
+    <OrderShopShow v-show="!loading" @success="loading = false" />
     <InnerLoading :loading="loading" />
   </div>
 </template>
 <script setup>
 import InnerLoading from 'src/components/ui/InnerLoading.vue'
-import OrderShop from 'src/components/shows/OrderShop.vue'
+import OrderShopShow from 'src/components/shows/OrderShopShow.vue'
 import { ref } from 'vue'
 
 const loading = ref(true)
